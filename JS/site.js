f6f64cacb29dee8d0ccc9913279eab4976948f3a
mateushbernardes <dopotshary@gmail.com>
2025-04-22 21:39:49 -0300

    js

---
+++ JS/site.js
@@ -0,0 +1,60 @@
+function validaFaleConosco() {
+    if (document.frmfaleconosco.txtnome.value=="") {
+        alert("Preencha o campo Nome");
+        document.frmfaleconosco.txtnome.focus();
+        return false;
+    }
+
+    if (document.frmfaleconosco.txtfone.value=="") {
+        alert("Preencha o campo telefone");
+        document.frmfaleconosco.txtfone.focus();
+        return false;
+    }
+
+    if (document.frmfaleconosco.txtemail.value=="") {
+        alert("Preencha o campo E-mail");
+        document.frmfaleconosco.txtemail.focus();
+        return false;
+
+    }
+    return true;
+}
+
+function verificaMotivo(motivo) {
+    var elemento = document.getElementById("opcaoProduto");
+
+    if(motivo=="PR") {
+        var select = document.createElement("select");
+        select.setAttribute("nome", "selproduto");
+
+        var option = document.createElement("option");
+        option.setAttribute("value", "");
+
+        var texto = document.createTextNode("Escolha");
+        option.appendChild(texto);
+
+        select.appendChild(option);
+
+        var option =document.createElement("option");
+        option.setAttribute("value", "FR");
+
+        var texto = document.createTextNode("Freezar");
+        option.appendChild(texto);
+
+        select.appendChild(option);
+
+        var option = document.createElement("option");
+        option.setAttribute("value", "GE");
+
+        var texto = document.createTextNode("Geladeira");
+        option.appendChild(texto);
+
+        select.appendChild(option);
+
+        elemento.appendChild(select);
+    } else {
+        if (elemento.firstChild) {
+            elemento.removeChild(elemento.firstChild);
+        }
+    }
+}
